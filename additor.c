0c6df2b21a738b81a926bdebede1e3fea4eb13d8
Kanaisabhi <ks8757579@gmail.com>
2025-02-14 15:15:11 +0530

    Update additor.c

--- additor.c
+++ additor.c
@@ -3,13 +3,13 @@
 #include <tgmath.h>
 
 int main(void) {
-    int num1;
-    int num2;
+    double num1;
+    double num2;
     printf("Enter you first number : ");
-    scanf("%d", &num1);
+    scanf("%lf", &num1);
     printf("Enter your second number : ");
-    scanf("%d", &num2);
-    printf("your answer is: %d", num1 + num2);
+    scanf("%lf", &num2);
+    printf("your answer is: %f", num1 + num2);
 
 
     return 0;
