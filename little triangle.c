db3af26393e6e5d82252b7309e5febacc5fc36f0
Kanaisabhi <ks8757579@gmail.com>
2025-02-14 13:26:48 +0530

    Create little triangle.c

---
+++ little triangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("      /|\n");
+    printf("     / | \n");
+    printf("    /  |  \n");
+    printf("   /___|   \n");
+    return 0;
+}
