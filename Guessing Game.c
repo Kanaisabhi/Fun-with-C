a50107554dafe1d4d6299293d9fbfc6509c7e13d
Kanaisabhi <ks8757579@gmail.com>
2025-02-14 19:39:22 +0530

    Create Guessing Game.c

---
+++ Guessing Game.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tgmath.h>
+
+int main(void){
+    int secretNumber = 7;
+    int guess;
+    int guessLimit = 0;
+    int guessCount = 3;
+    while (guess != secretNumber) {
+        if (guessCount > guessLimit) {
+            printf("please enter you guessed no.: ");
+            scanf("%d", &guess);
+            guessCount--;
+        }else if (guessCount == guessLimit) {
+            printf("You are out of guesses!\n");
+            break;
+        }
+
+    }
+    if (guess == secretNumber) {
+        printf("you win!!" );
+    }else {
+        printf("you lost!");
+    }
+    return 0;
+}
