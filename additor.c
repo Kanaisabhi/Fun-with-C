64c534b93e108a9faf242d77359868fa565c2239
Kanaisabhi <ks8757579@gmail.com>
2025-02-14 15:11:45 +0530

    Create additor.c

---
+++ additor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tgmath.h>
+
+int main(void) {
+    int num1;
+    int num2;
+    printf("Enter you first number : ");
+    scanf("%d", &num1);
+    printf("Enter your second number : ");
+    scanf("%d", &num2);
+    printf("your answer is: %d", num1 + num2);
+
+
+    return 0;
+}
